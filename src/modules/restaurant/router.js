43be2edb5163201f7cbb72c7691c711496f83455
DaddyTrap <914670787@qq.com>
2018-06-27 18:04:21 +0800

    :frog: 添加餐馆api的router

---
+++ src/modules/restaurant/router.js
@@ -0,0 +1,17 @@
+import Router from 'koa-express-router';
+
+import { createRestaurant, retrieveAllRestaurants, retrieveOneRestaurant, deleteOneRestaurant, updateOneRestaurant } from './controller';
+import { parseRestaurant } from './controller';
+
+const restaurantRouter = new Router();
+
+restaurantRouter.route('/').get(retrieveAllRestaurants)
+                           .post(createRestaurant);
+
+restaurantRouter.route('/:restaurant_id').get(retrieveOneRestaurant)
+                                         .put(updateOneRestaurant)
+                                         .delete(deleteOneRestaurant);
+
+restaurantRouter.param('restaurant_id', parseRestaurant);
+
+export default restaurantRouter;
